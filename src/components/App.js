befcf661f678ed25100410f23124128bf2382b01
Yong Wang <yongwangd@gmail.com>
2018-01-02 17:29:20 -0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import '../assets/css/App.css';
+import React, { Component } from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, Electron!</h1>
+        <p>
+          I hope you enjoy using basic-electron-react-boilerplate to start your
+          dev off right! Hope it work. and it does
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
